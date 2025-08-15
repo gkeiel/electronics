a6351c8aa41101c9f105a2e9ef7362b7b6c15dc3
Guilherme Keiel <gkeiel@hotmail.com>
2025-08-15 10:20:39 -0300

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# electronics
+# diode_real_modelling_solver
 
+A brief code to compare the solution of a simple source-diode-resistor circuit considering both the simplified and the real diode model.
---
+++ diode_circuit_solver/diode_real_modelling_solution.m
@@ -0,0 +1,36 @@
+clc; clear;
+
+% constants of circuit with KVL: E -V_D -IR = 0
+E = 10;
+R = 100;
+
+% diode parameters
+I_S = 10*10^-12;
+k   = 1.38*10^-23;
+q   = 1.6*10^-19;
+T_K = 273 +25;
+n   = 1;
+V_T = k*T_K/q;
+par = [E; R; I_S; n; V_T];
+
+% simplified model solution
+V_D = 0.7;
+I_D = (E -V_D)/R;
+
+% solution of both I_D = I_S*( exp(V_D/(n*V_T)) -1 ) and I_D = (E -V_D)/R;
+f   = @(x) nonlinear_system(x, par);   % one can define another circuit equation inside function
+x_0 = [.1, .1];                        % initial guess
+options = optimset('Display','iter');
+[x, fval] = fsolve(f, x_0, options);
+
+% result comparison
+fprintf('V_D using simplified model = %.3f V\n', V_D)
+fprintf('I_D using simplified model = %.3f A\n', I_D)
+fprintf('V_D using real model = %.3f V\n', x(1))
+fprintf('I_D using real model = %.3f A\n', x(2))
+
+% plot
+figure(1)
+plot(t,r,t,v_o,t,i_o)
+legend('r(t) [V]','v_o(t) [V]','i_o(t) [A]');
+xlabel('Time (s)'); ylabel('Amplitude');
